692915e5844e9002b4fae7d235179e9c3c85b3cc
Paweł Połczyński <pawel@polczynski.com>
2015-06-07 21:49:26 +0200

    Time server done! or is it?

---
+++ learnyounode/010_TimeServer/program.js
@@ -0,0 +1,34 @@
+var net = require('net');
+
+if(process.argv.length > 2) {
+	var server = net.createServer(timeServer);
+	server.listen(process.argv[2]);
+} 
+else {
+	console.log("specify port");
+}
+
+function timeServer (socket) {
+	socket.end(getNow());
+}
+
+function getNow() {
+	var delimiterDate = '-';
+	var delimiterTime = ':';
+	var delimiterDateTime = ' ';
+	var curentTime = new Date();
+
+	return 	curentTime.getFullYear().toString() +
+		delimiterDate + 
+		firstZero(curentTime.getMonth() + 1) + 
+		delimiterDate + 
+		firstZero(curentTime.getDate()) +
+		delimiterDateTime +
+		firstZero(curentTime.getHours()) + 
+		delimiterTime + 
+		firstZero(curentTime.getMinutes())
+}
+
+function firstZero (toAppend) {
+	return (toAppend < 10 ? '0' : '') + toAppend.toString();
+}
